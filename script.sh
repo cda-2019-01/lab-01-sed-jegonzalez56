5fb92fde17d34045684f9ad7f3bdb8d0b3cb4268
jegonzalez56 <jegonzalezca@unal.edu.co>
2019-05-13 19:12:41 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,5 @@
-# Escriba su código aquí
+#!/bin/bash
+
+sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/20\3-\2-\1/' data.csv | sed 's/;;/;\\N;/' | sed 's/;N;/;\\N;/' | sed 's/;/,/g' | sed 's/,/./4' | sed 'y/ac/AC/' | sed '/\\N/d'
+
+exit
